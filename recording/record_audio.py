5c4eef2c82f1c3bfec805505ded9819fea152cc1
mariuslang <marius@mariuslang.de>
2025-11-21 21:47:23 +0100

    add simple script to record audio

---
+++ recording/record_audio.py
@@ -0,0 +1,29 @@
+import sounddevice as sd
+import wave
+import datetime
+
+samplerate = 16000  # Hz
+channels = 1
+
+if __name__ == '__main__':
+    filename = f'recording_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.wav'
+
+    print(f'Recording ... Press CTRL+C to stop.\nFile: {filename}')
+
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(2)  # 16-bit
+    wf.setframerate(samplerate)
+
+    try:
+        with sd.InputStream(samplerate=samplerate, channels=channels, dtype='int16') as stream:
+            while True:
+                data = stream.read(1024)[0]
+                wf.writeframes(data.tobytes())
+
+    except KeyboardInterrupt:
+        print('\nStopped.')
+
+    finally:
+        wf.close()
+        print('File saved.')
